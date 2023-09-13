dc255c3692b3317172b03e5877436e1194ef0e78
sdemicco <sildemicco@gmail.com>
2023-09-13 13:17:55 -0300

    subir archivos para funciones

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ app = FastAPI( title= "Consulta base de datos Steam Games",
               description= "Permite realizar consultas sobre Steam-games")
 
 # Leer los archivos .parquet para el consumo de la API
-#df_userdata1 = pd.read_csv("userdata_1.csv")
-#df_userdata2 = pd.read_csv("userdata_2.csv")
+df_userdata1 = pd.read_csv("userdata_1")
+df_userdata2 = pd.read_csv("userdata_2")
 
 @app.get("/")
 async def ruta_prueba():
@@ -18,11 +18,11 @@ async def ruta_prueba():
 
 # Ruta de cantidad de filmaciones para un determinado mes 
 @app.get("/countreviews/{user_id}", name = "countreviews (user_id)")
-async def countreviews(usuario:str):
+async def countreviews(user_id:str):
     '''xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx.'''
-    user = user_id
-    df1_filtrado = df_userdata1[df_userdata1['user_id'] == user]
-    df2_filtrado = df_userdata2[df2_userdata2['user_id'] == user]
+    usuario=user_id
+    df1_filtrado = df_userdata1[df_userdata1['user_id'] == usuario]
+    df2_filtrado = df_userdata2[df_userdata2['user_id'] == usuario]
     valor1=df1_filtrado.gasto.iloc[0]
     valor2=df2_filtrado.porcentaje.iloc[0]
     return  {'dinero gastado' : valor1,'porcecntaje_recom':valor2}
